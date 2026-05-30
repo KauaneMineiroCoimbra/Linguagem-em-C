9aec2838fe72f57a9ba3e84a8569c3c712ba781a
Kauane Mineiro Coimbra <kauanemineirok@gmail.com>
2026-05-30 16:44:00 -0300

    Create Exercicio04.c

---
+++ Exercicio04.c
@@ -0,0 +1,15 @@
+//4 – compras no exterior ativ.4
+#include <stdio.h>
+
+int main() {
+    float real, dolar;
+
+    scanf("%f", &real);
+
+    dolar = real / 5.42;
+
+    printf("Valor em real R$ %.2f.\n", real);
+    printf("Valor em dólar $ %.2f.\n", dolar);
+
+    return 0;
+}
